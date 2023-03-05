d317b2553f4e8cd696143c6e71193a6b19be0a85
GurgenBayramyan <gugobayramyan@gmail.com>
2023-03-06 00:52:11 +0400

    Gurgen firs commit

---
+++ src/SearchBlock.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SearchBlock() {
+  return (
+    <View>
+      <Text>SearchBlock</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
