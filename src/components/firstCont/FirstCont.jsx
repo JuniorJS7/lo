742c8a9af201136b0982c649fcedd69d93e156d4
VilenAydinyan <125986907+VilenAydinyan@users.noreply.github.com>
2023-03-07 19:04:39 +0400

    Vilen (#3)
    * update
    
    * create container

---
+++ src/components/firstCont/FirstCont.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './FirstCont.css'
+
+function FirstCont(props) {
+    return (
+        <div className='cont'>
+            <div className="text">
+                <p><span>BRANNOR  - </span> &nbsp;&nbsp; <br/>  ПОДУКТ <br />  НОВЫЙ ЭПОХИ</p>
+                <div className="text2">
+                    <p>Лучшие фрикционные материалы для тормозных колодок и дисков, основанные на самых передовых стандартах современной автомобильной промышленности</p>
+                    <button>Узнать больше о компании</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FirstCont;
