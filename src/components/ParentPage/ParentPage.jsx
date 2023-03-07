985fb434c748d0fa3b2591d22c0b650a5bb3629b
VilenAydinyan <aydinyanvilen@gmail.com>
2023-03-07 18:41:39 +0400

    create container

--- src/components/ParentPage/ParentPage.jsx
+++ src/components/ParentPage/ParentPage.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import NavBar from '../NavBar/NavBar';
-import Selectors from '../Selectors/Selectors';
-
+import React from "react";
+import NavBar from "../NavBar/NavBar";
+import Selectors from "../Selectors/Selectors";
+import FirstCont from "../firstCont/FirstCont";
 
 function ParentPage(props) {
-    return (
-        <div>
-            <NavBar />
-            <Selectors />
-        </div>
-    );
+  return (
+    <div>
+      <NavBar />
+      <Selectors />
+      <FirstCont />
+    </div>
+  );
 }
 
 export default ParentPage;
