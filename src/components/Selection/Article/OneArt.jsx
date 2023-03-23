c3acea198fad9869fbb44def48de7f0610c52719
AidaMkrtchyan <aida.mkrtchyan0606@gmail.com>
2023-03-23 12:09:54 +0400

    Add media OneArt

--- src/components/Selection/Article/OneArt.jsx
+++ src/components/Selection/Article/OneArt.jsx
@@ -1,24 +1,26 @@
-import React from "react";
-import "./OneArt.css";
+import React from 'react';
+import './OneArt.css';
+import union from './img/union.png';
 
 export default function OneArt() {
-  return (
-    <div className="ArtOne">
-      <div className="cont">
-        <div className="text">
-          <p>
-            <span>BRANNOR - </span> ПОДУКТ <br /> НОВЫЙ ЭПОХИ
-          </p>
-          <div className="text2">
-            <p>
-              Лучшие фрикционные материалы для тормозных колодок и дисков,
-              основанные на самых передовых стандартах современной автомобильной
-              промышленности
-            </p>
-            <button>Узнать больше о компании</button>
-          </div>
-        </div>
+   return (
+      <div className='OneArt'>
+       <div className='OneArt__container'>
+           <div className='OneArt__content'>
+              <h1 className='OneArt__title'>
+                 <span>BRANNOR - </span> ПОДУКТ <br /> НОВЫЙ ЭПОХИ
+              </h1>
+                 <p className='OneArt__descr'>
+                    Лучшие фрикционные материалы для тормозных колодок и дисков,
+                    основанные на самых передовых стандартах современной
+                    автомобильной промышленности
+                 </p>
+                 <button className='OneArt__button'>Узнать больше о компании</button>
+           </div>
+           <div className='OneArt__img'>
+            <img src={union} alt="" />
+           </div>
+       </div>
       </div>
-    </div>
-  );
+   );
 }
--- src/components/footer/footFinish/FooterFinish.jsx
+++ src/components/footer/footFinish/FooterFinish.jsx
@@ -7,14 +7,18 @@ import Group from './img/Group.svg';
 
 export default function FooterFinish() {
   return (
-    <div className='Foot'>
+    <div className='Footer'>
       <div className="container">
         <div className="list">
-          <div className="item-1">
-            <img className='logos' src={logos} alt="" />
-            <a className='link' href='##' style={{textIndent: "-100px"}}>Политика конфиденциальности</a>
-            <a className='link' href='##' style={{textIndent: "-110px"}}>Пользовательское соглашение</a>
-            <a className='link' href='##'>Согласие на обработку персональных данных</a>
+          <div className="item">
+            <div>
+              <img className='logos' src={logos} alt="" />
+            </div>
+           <div className='item'>
+              <a  href='##'>Политика конфиденциальности</a>
+              <a  href='##'>Пользовательское соглашение</a>
+              <a  href='##'>Согласие на обработку персональных данных</a>
+           </div>
           </div>
           <div className="item">
             <a href="##">Контакты</a>
@@ -29,10 +33,16 @@ export default function FooterFinish() {
             <a href="##">Обучение</a>
           </div>
           <div className="item">
-            <img src={phone} alt=""/>
-            <div className='item-tell'><a href="tel:+74951252300">+7 495 125-23-00</a></div>
-            <img src={Group} alt=""/>
-            <p>г. Москва, МКАД 32й км, строение 4</p>
+            <div className='item-block'>
+              <div className='item-box'>
+                <img src={phone} alt=""/>
+                <a className='item-tell' href="tel:+74951252300">+7 495 125-23-00</a>
+              </div>
+              <div className='item-box'>
+                <img src={Group} alt=""/>
+                <p>г. Москва, МКАД 32й км, строение 4</p>
+              </div>
+            </div>
           </div>
           </div>
           <div className='copyRight'>Brannor.ru © 2023 • Официальный представитель BRANNOR в России
